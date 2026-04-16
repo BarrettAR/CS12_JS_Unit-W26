70125a892376ac6e85314de7b0079a6ffd46b23f
BarrettAR <Barrett.alex08@gmail.com>
2026-04-16 11:26:35 -0300

    Finished L1 started L2

--- L2 - Data and Variables/Task2_1.js
+++ L2 - Data and Variables/Task2_1.js
@@ -1,14 +1,21 @@
 // Task 2.1: Simple Variable Practice
 // Part 1 Working with Variables
 // 1. Declare two variables: admin and firstName.
+let admin;
 
+let firstName;
 // 2. Assign the value "John" to name.
-
+firstName = "John";
 // 3. Copy the value from name to admin.
-
+admin = firstName;
 // 4. Show the value of admin using console.log (must output “John”).
-
+console.log(admin);
 // Part 2 Giving the Right Name
 // 1. Create a variable with the name of our planet. How would you name such a variable?
-
+let homePlanet = "Earth";
 // 2. Create a variable to store the name of a current visitor to a website. How would you name that variable?
+let userName = "Alex";
+
+console.log(Number("12.2"));
+let age = Number(prompt("What is your age?"));
+console.log("$" + String(9));
